1bef721efd033d4c67fb76fc5d5a01fcf5320dd3
Joshua Barua <joshuabarua9@hotmail.com>
2024-05-17 15:34:10 +0200

    Add  form and env variables for  firebase

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,27 +1,29 @@
-// import React, {useState} from 'react';
-// import {db} from './store/firebase';
-// import {useHistory} from 'react-router-dom';
+import React, {useState} from 'react';
+import {db} from './store/firebase';
+import {useNavigate} from 'react-router-dom';
+import {collection, addDoc} from 'firebase/firestore';
 
-// const Form = () => {
-// 	const [name, setName] = useState('');
-// 	const [country, setCountry] = useState('');
-// 	const history = useHistory();
+const Form = () => {
+	const [name, setName] = useState('');
+	const [country, setCountry] = useState('');
 
-// 	const handleSubmit = async (e) => {
-// 		e.preventDefault();
-// 		await db.collection('participants').add({name, country});
-// 		history.push('/participants');
-// 	};
+	const navigate = useNavigate();
 
-// 	return (
-// 		<form onSubmit={handleSubmit}>
-// 			<TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} required />
-// 			<TextField label="Country of Origin" value={country} onChange={(e) => setCountry(e.target.value)} required />
-// 			<Button type="submit" variant="contained" color="primary">
-// 				Submit
-// 			</Button>
-// 		</form>
-// 	);
-// };
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+		e.preventDefault();
+		await addDoc(collection(db, 'lucky-draw'), {name, country});
+		navigate('/');
+	};
 
-// export default Form;
+	return (
+		<form onSubmit={handleSubmit}>
+			<TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} required />
+			<TextField label="Country of Origin" value={country} onChange={(e) => setCountry(e.target.value)} required />
+			<Button type="submit" variant="contained" color="primary">
+				Submit
+			</Button>
+		</form>
+	);
+};
+
+export default Form;
